51fdf9e97d4b802539866807d5c0bc1317e405a4
Gabriel Montplaisir <gabriel.montplaisir@protonmail.com>
2024-06-26 20:49:34 -0400

    Create Login Functionality

---
+++ pawpals/src/main/java/com/pawpals/servlets/LoginServlet.java
@@ -0,0 +1,75 @@
+package com.pawpals.servlets;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.pawpals.dao.ApplicationDao;
+import com.pawpals.dao.UserDao;
+import com.pawpals.interfaces.FormValidation;
+import com.pawpals.session.Session;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends FormValidation {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String message;
+		RequestDispatcher dispatcher = null;
+		ApplicationDao.dao.createDatabase();
+		ApplicationDao.dao.createUserTable();
+		message = validateForm(req.getParameterMap());
+		if (message != null) {
+			req.setAttribute("message", message);
+			dispatcher = req.getRequestDispatcher("index.jsp");
+			dispatcher.forward(req, resp);
+			return;
+		}
+		
+
+		UserDao.userDao.authenticateUser(req);
+		
+		if (Session.session.getUser() != null) {
+			HttpSession session = req.getSession();
+			session.setAttribute("session", Session.session);
+			resp.sendRedirect("./dashboard/createwalk.jsp");
+			return;
+		} else {
+			req.setAttribute("message", "Cannot find user with this email address. Please register for an account.");
+		}
+	}
+	
+	@Override
+	protected String validateForm(Map<String,String[]> params) {
+		Iterator<Entry<String, String[]>> iterator = params.entrySet().iterator();
+		String email = null;
+		while (iterator.hasNext()) {
+			Map.Entry<String, String[]> entry = (Entry<String, String[]>)iterator.next();
+			String paramName = entry.getKey();
+			String paramValue = entry.getValue()[0];
+
+			
+			if (isEmpty(paramName)) {return paramName+" cannot be empty.";}
+			if (paramName.equals("email")) {
+				if (!paramValue.matches(emailRegEx)) return "Email is invalid.";
+				if (!UserDao.userDao.userExists(paramValue)) return "Cannot find user with this email address.";
+				email = paramValue;
+			}
+			
+			if (paramName.equals("password")) {
+				if (!UserDao.userDao.passwordMatches(email, paramValue)) return "Email & Password do not match. Please try again.";
+			}
+				
+		}
+		return null;
+	}
+}
