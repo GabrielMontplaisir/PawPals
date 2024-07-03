659edceb5944c5a14bffc486b2e8d12c8b68fae8
Gabriel Montplaisir <gabriel.montplaisir@protonmail.com>
2024-07-03 07:11:13 -0400

    Cleaned up DogDao

--- pawpals/src/main/java/com/pawpals/dao/DogDao.java
+++ pawpals/src/main/java/com/pawpals/dao/DogDao.java
@@ -6,16 +6,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.pawpals.beans.Dog;
 
 public class DogDao {
     public static final DogDao dogDao = new DogDao();
-    private final String DOG_ID = "dog_id";
-    private final String OWNER_ID = "owner_id";
-    private final String NAME = "name";
-    private final String SIZE = "size";
-    private final String SPECIAL_NEEDS = "special_needs";
-    private final String IMMUNIZED = "immunized";
+    public static final String DOG_ID = "dog_id";
+    public static final String OWNER_ID = "owner_id";
+    public static final String NAME = "name";
+    public static final String SIZE = "size";
+    public static final String SPECIAL_NEEDS = "special_needs";
+    public static final String IMMUNIZED = "immunized";
 
     private DogDao() {}
     
@@ -28,12 +31,19 @@ public class DogDao {
             stmt.setInt(1, dogId);
             ResultSet rs = stmt.executeQuery();
             rs.next();
-            int ownerId = rs.getInt("owner_id");
-            String dogName = rs.getString("name");
-            String dogSize = rs.getString("size");
-            String dogSpecialNeeds = rs.getString("special_needs");
-            Boolean immunized = rs.getBoolean("immunized");
-            return new Dog(dogId, ownerId, dogName, dogSize, dogSpecialNeeds, immunized);
+            
+            Dog dog = new Dog(
+            		dogId, 
+            		rs.getInt(OWNER_ID), 
+            		rs.getString(NAME), 
+            		rs.getString(SIZE), 
+            		rs.getString(SPECIAL_NEEDS), 
+            		rs.getBoolean(IMMUNIZED)
+            );
+            
+            rs.close();
+            
+            return dog;
         } catch (SQLException e) {
             DBUtil.processException(e);
         } catch (ClassNotFoundException e) {
@@ -43,7 +53,11 @@ public class DogDao {
     	return null;
     }
 
-    public void addDog(int ownerId, String name, String size, String specialNeeds, boolean immunized) {
+    public void addDog(int ownerId, HttpServletRequest req) {
+        String name = req.getParameter("name");
+        String size = req.getParameter("size");
+        String specialNeeds = req.getParameter("specialneeds");
+        boolean immunized = req.getParameter("immunized") != null;
         String sql = "INSERT INTO " + ApplicationDao.DOGS_TABLE + " (" + OWNER_ID + "," + NAME + "," + SIZE + "," + SPECIAL_NEEDS + "," + IMMUNIZED + ") VALUES (?, ?, ?, ?, ?)";
 
         try (
@@ -65,9 +79,9 @@ public class DogDao {
         }
     }
 
-    public List<Dog> getDogsByOwnerUserId(int userId) {
+    public List<Dog> getDogsByOwner(int userId) {
         List<Dog> dogs = new ArrayList<>();
-        String sql = "SELECT " + DOG_ID + ", " + OWNER_ID + ", " + NAME + ", " + SIZE + ", " + SPECIAL_NEEDS + ", " + IMMUNIZED + " FROM " + ApplicationDao.DOGS_TABLE + " WHERE " + OWNER_ID + " = ?";
+        String sql = "SELECT * FROM " + ApplicationDao.DOGS_TABLE + " WHERE " + OWNER_ID + " = ?";
 
         try (
                 Connection conn = DBConnection.getDBInstance();
@@ -79,9 +93,12 @@ public class DogDao {
 
             while (rs != null && rs.next()) {
                 Dog dog = new Dog(
-                    rs.getInt(DOG_ID),				rs.getInt(OWNER_ID),
-                    rs.getString(NAME),				rs.getString(SIZE),
-                    rs.getString(SPECIAL_NEEDS),	rs.getBoolean(IMMUNIZED)
+                    rs.getInt(DOG_ID),
+                    rs.getInt(OWNER_ID),
+                    rs.getString(NAME),
+                    rs.getString(SIZE),
+                    rs.getString(SPECIAL_NEEDS),
+                    rs.getBoolean(IMMUNIZED)
                 );
                 dogs.add(dog);
             }
