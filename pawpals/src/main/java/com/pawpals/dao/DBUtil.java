0c6091ba3aa834f0740864e95c19e2d82508f9b1
Jeff Spidle <jeff@spidle.ca>
2024-07-04 17:22:00 -0400

    reject walk offer, start walk, complete walk, add dashboard sections

--- pawpals/src/main/java/com/pawpals/dao/DBUtil.java
+++ pawpals/src/main/java/com/pawpals/dao/DBUtil.java
@@ -32,18 +32,29 @@ public class DBUtil {
 			return null;
 		}
 	}
-	
+
 	public static void setConnStr() {
-		if (M_CONN_STRING.contains(ApplicationDao.DB_NAME)) return;
+		if (M_CONN_STRING.contains(ApplicationDao.DB_NAME))
+			return;
 		M_CONN_STRING += ApplicationDao.DB_NAME;
 		H_CONN_STRING += ApplicationDao.DB_NAME;
 	}
-	
+
 	public static void processException(SQLException e) {
-		System.err.println("Error message: "+e.getMessage());
-		System.err.println("Error code: "+e.getErrorCode());
-		System.err.println("SQL State: "+e.getSQLState());
+		System.err.println("Error message: " + e.getMessage());
+		System.err.println("Error code: " + e.getErrorCode());
+		System.err.println("SQL State: " + e.getSQLState());
+		System.err.println("Trace project package: " + printRelevantStackTrace(e) + "\n");
 	}
-	
-	
+
+	private static String printRelevantStackTrace(Throwable e) {
+		int i = 0;
+		for (StackTraceElement element : e.getStackTrace()) {
+			i++;
+			if (element.getClassName().startsWith("com.pawpals")) {
+				return ((String) "Relevant stack trace element[" + i + "]: " + element);
+			}
+		}
+		return "not found";
+	}
 }
