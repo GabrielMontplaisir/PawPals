7c9f2ceb0b6f252d6e0c15eb4b4056e98de8bbec
Jeff Spidle <jeff@spidle.ca>
2024-07-04 17:24:45 -0400

    reject offer, start/finish walks as walker, dashboard sections

---
+++ pawpals/src/main/java/com/pawpals/servlets/walker/CompleteWalkServlet.java
@@ -0,0 +1,46 @@
+package com.pawpals.servlets.walker;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.pawpals.beans.User;
+import com.pawpals.beans.Walk;
+import com.pawpals.dao.WalkDao;
+import com.pawpals.interfaces.WalkStatus;
+import com.pawpals.services.SessionService;
+
+@WebServlet("/dashboard/complete-walk-walker")
+public class CompleteWalkServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	User user = SessionService.srv.getSessionUser(req);
+    	if (user == null) { 
+    		resp.sendRedirect("../index.jsp");
+    		return;
+    	}
+
+        int walkId = Integer.parseInt(req.getParameter("id"));
+        Walk walk = WalkDao.dao.getWalkById(walkId);
+
+        if (walk == null || walk.getWalkerId() != user.getId()) {
+        	System.out.println("Error: Could not complete walk. Walk not found or user not walker.");
+            resp.sendRedirect("./walker.jsp");
+            return;
+        }
+        
+              
+        walk.setStatus(WalkStatus.WALKER_COMPLETED);
+        WalkDao.dao.setStatus(walk.getWalkId(), walk.getStatus());
+        resp.sendRedirect("./walkdetails?id="+walkId);
+    }
+    
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	doPost(req, resp);
+    }
+    
+}
+
