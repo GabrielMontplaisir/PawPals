1238598bce2a7d1b8e3e2d2890ef3c2316292b70
Gabriel Montplaisir <gabriel.montplaisir@protonmail.com>
2024-07-15 14:21:54 -0400

    Created JSP pages Servlets

---
+++ pawpals/src/main/java/com/pawpals/servlets/pages/OwnerServlet.java
@@ -0,0 +1,41 @@
+package com.pawpals.servlets.pages;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pawpals.beans.Dog;
+import com.pawpals.beans.User;
+import com.pawpals.beans.Walk;
+import com.pawpals.dao.DogDao;
+import com.pawpals.dao.WalkDao;
+import com.pawpals.services.SessionService;
+
+@WebServlet("/dashboard/owner")
+public class OwnerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = SessionService.srv.getSessionUser(req);
+		
+		if (user == null) {
+			resp.sendRedirect("../");
+			return;
+		}
+		
+		List<Dog> dogs = DogDao.dogDao.getDogsByOwner(user.getId());
+		List<Walk> walks = WalkDao.dao.getWalksByOwnerId(user.getId());
+		
+		req.setAttribute("dogs", dogs);
+		req.setAttribute("walks", walks);		
+		
+		req.getRequestDispatcher("owner.jsp").forward(req, resp);
+	}
+
+}
---
+++ pawpals/src/main/java/com/pawpals/servlets/pages/WalkerServlet.java
@@ -0,0 +1,42 @@
+package com.pawpals.servlets.pages;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pawpals.beans.User;
+import com.pawpals.beans.Walk;
+import com.pawpals.dao.WalkDao;
+import com.pawpals.services.SessionService;
+
+@WebServlet("/dashboard/walker")
+public class WalkerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = SessionService.srv.getSessionUser(req);
+		
+		if (user == null) {
+			resp.sendRedirect("../");
+			return;
+		}
+		
+		HashMap<Integer, Boolean> walkOffers = new HashMap<>();
+		List<Walk> walks = (List<Walk>) WalkDao.dao.getWalksPostedForReceivingOffers(user.getId(), walkOffers);
+		List<Walk> userWalks = (List<Walk>) WalkDao.dao.getWalksByWalkerId(user.getId());
+		
+		req.setAttribute("walks", walks);		
+		req.setAttribute("walkOffers", walkOffers);
+		req.setAttribute("userWalks", userWalks);
+		
+		req.getRequestDispatcher("walker.jsp").forward(req, resp);
+	}
+
+}
